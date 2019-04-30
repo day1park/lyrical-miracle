63cb8fbcc4e8730ab520b9a7f26286ffc531145c
day1park <daypark2408@gmail.com>
2019-04-30 14:40:11 +1000

    import react router and create index and tracks components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <h1>hello universe</h1>
-    </div>
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Index} />
+          </Switch>
+        </div>
+      </React.Fragment>
+    </Router>
   );
 }
 
---
+++ src/components/tracks/Tracks.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Tracks extends Component {
+  render() {
+    return (
+      <div>
+        <h1>tracky dacks</h1>
+      </div>
+    );
+  }
+}
+
+export default Tracks;
