1cc742eb1d93b4982286110bff9df97f7b3a9c74
day1park <daypark2408@gmail.com>
2019-05-01 18:02:42 +1000

    create Lyric component and add path to lyrics page in app.js file

---
+++ src/components/tracks/Lyrics.js
@@ -0,0 +1,14 @@
+// page for the route to lyrics
+import React, { Component } from "react";
+
+class Lyrics extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Lyrics yo</h1>
+      </div>
+    );
+  }
+}
+
+export default Lyrics;
